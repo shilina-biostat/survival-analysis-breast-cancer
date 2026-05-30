b589591b238e2192aeff8a110e3bfee084cf67fa
shilina-biostat <biostat.nsh@gmail.com>
2026-05-30 16:04:17 +0200

    Update 01_data_cleaning.R

--- scripts/01_data_cleaning.R
+++ scripts/01_data_cleaning.R
@@ -1,9 +1,15 @@
+# Load libraries
 library(readr)
+library(dplyr)
 
+# Import dataset
 data <- read_csv("clinical_data(labels).csv")
 
+# Convert survival time to years
 data$Time_years <- data$Time / 365
 
+# Recode survival status
 data$status <- ifelse(data$vital_status == 2, 1, 0)
 
+# Summary statistics
 summary(data)
